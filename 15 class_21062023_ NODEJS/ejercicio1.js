980e511ecf6ad68e84cd28befbcfa17331655662
Paola Beltran <pavis.asajfb@gmail.com>
2023-06-21 21:30:32 -0500

    ejercicios con NODE.js

---
+++ 15 class_21062023_ NODEJS/ejercicio1.js
@@ -0,0 +1,10 @@
+//aqua
+let cantidadaqua = 2;
+let aqua = 200;
+
+if(aqua > 0){
+    aqua = aqua * cantidadaqua;    
+    console.log(aqua);
+}else{
+    console.log("dato incorrecto");
+}
---
+++ 15 class_21062023_ NODEJS/script.js
@@ -0,0 +1,8 @@
+const PI = 3.1416;
+//const r = prompt("dame un valor de radio");
+const r = 2;
+let areaCirculo = PI * (r**2);
+console.log(areaCirculo);
+//document.write(areaCirculo);
+//alert(areaCirculo);
+
